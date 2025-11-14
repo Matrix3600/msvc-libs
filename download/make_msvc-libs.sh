cdf38472eba1a12e7e3dfa825ccfa2c779e99f25
Matrix3600 <matrix3600x@gmail.com>
2025-11-14 11:59:52 +0100

    Change version to 1.1

--- download/make_msvc-libs.sh
+++ download/make_msvc-libs.sh
@@ -4,7 +4,7 @@
 # make_msvc-libs.sh
 #
 # -----------------------------------------------------------------------------
-# Copyright 2025 https://github.com/Matrix3600
+# Copyright 2025-2026 https://github.com/Matrix3600
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
